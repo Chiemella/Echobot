ad2b455515200835c013dcba02f6c9b7a4d74849
chiemella <williamschiemelaod@gmail.com>
2026-09-06 20:14:56 +0000

    made user id copyable

--- GGJGMbot.py
+++ GGJGMbot.py
@@ -210,9 +210,10 @@ async def notify_admin(
         text=(
             "📢 USER ACTIVITY\n\n"
             f"👤 User: {username}\n"
-            f"🆔 User ID: {user.id}\n"
+            f"🆔 User ID: <code>{user.id}</code>\n"
             f"⚡ Command: {command}"
         )
+	parse_mode="HTML"
     )
 
 # Withdraw command - Step 1: Ask for wallet address
